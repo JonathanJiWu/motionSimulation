14e29525cd01acb241d4c3f5e52d99020eb9eb98
JonathanJiWu <87452633+JonathanJiWu@users.noreply.github.com>
2025-07-15 14:48:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,20 @@
 #include <iostream>
 #include <iomanip>
 
-struct MotionEvent {
+class MotionEvent {
+public:
     double time;
     double position;
     double velocity;
 
-    friend std::ostream& operator<<(std::ostream& os, const MotionEvent& ev) //what does this mean? => an object that's easy to cout? => how can we use it? 
-    {
-        return os << "t=" << std::fixed << std::setprecision(2) << ev.time
-                  << "  x=" << ev.position << "  v=" << ev.velocity;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const MotionEvent& ev);
 };
+
+std::ostream& operator<<(std::ostream& os, const MotionEvent& ev)
+{
+    return os << "t=" << std::fixed << std::setprecision(2) << ev.time
+        << "  x=" << ev.position << "  v=" << ev.velocity;
+}
 
 // Generate a discrete list of events (simulated motion)
 std::vector<MotionEvent> Simulate1DMotion
@@ -40,7 +43,7 @@ std::vector<MotionEvent> Simulate1DMotion
         currentTime += timeStep;
         result.push_back({currentTime, position, velocity});
     }
-
+    
     return result;
 }
 
@@ -60,32 +63,34 @@ std::vector<MotionEvent> FilterByVelocity(const std::vector<MotionEvent>& events
 // 2. Return a new vector sorted by descending |velocity|
 std::vector<MotionEvent> SortByAbsoluteVelocityDesc(const std::vector<MotionEvent>& events)
 {
+    return {};
 
 }
 
 // 3. Compute total change in position from the first to last event
 double ComputeTotalDisplacement(const std::vector<MotionEvent>& events)
 {
-
+    return {};
 }
 
 
 // 4. Use ranges to find maximum speed (abs velocity)
 double MaxSpeed(const std::vector<MotionEvent>& events)
 {
-
+    return {};
 }
 
 
 // 5. Return vector<string> formatted as CSV: "time,position,velocity"
 std::vector<std::string> FormatAsCSV(const std::vector<MotionEvent>& events)
 {
-
+    return {};
 }
 
 
 
 int main()
 {
-    Simulate1DMotion(0, 0, 4, 0.1, 10);
+    //Simulate1DMotion(0, 0, 4, 0.1, 10);
+	//std::cout << Simulate1DMotion(0, 0, 4, 0.1, 10); //this is a vector of MotionEvent s have to loop through it and print one by one
 }
